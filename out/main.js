fc9775ffd0924b122d074afaf43c3a07eecd4c66
Devan Buggay <sbuggay@gmail.com>
2018-01-07 17:27:29 -0800

    Add more functionality

--- out/main.js
+++ out/main.js
@@ -1,12 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const inquirer = require("inquirer");
 const request = require("request");
 const fs = require("fs");
+const inquirer = require("inquirer");
 const path_1 = require("path");
 const APP_ID = 730;
 const API_KEY = "1578879989BD74A6D189050250810E86";
-const bit = 61197960265728;
+const BIT_SHIFT = 61197960265728;
+const PLAYER_SUMMARIES_API_URL = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002";
+const resources = {
+    import: "import",
+    export: "export",
+    sync: "sync"
+};
+var applicationOptions;
+(function (applicationOptions) {
+    applicationOptions["IMPORT"] = "IMPORT";
+    applicationOptions["IMPORTWEB"] = "IMPORTWEB";
+    applicationOptions["EXPORT"] = "EXPORT";
+    applicationOptions["SYNC"] = "SYNC";
+})(applicationOptions || (applicationOptions = {}));
 /**
  * Utility function to convert 32bit steamids to 64bit
  *
@@ -14,8 +27,9 @@ const bit = 61197960265728;
  * @returns
  */
 function convertTo64BitId(id) {
-    return `765${Number(id) + bit}`;
+    return `765${Number(id) + BIT_SHIFT}`;
 }
+// TODO: Add more search paths
 const userDataPath = "C:/Program Files (x86)/Steam/userdata";
 const cfgRelativePath = `${APP_ID}/local/cfg`;
 const configFiles = [
@@ -32,7 +46,7 @@ const configFiles = [
 function getPlayerSummaries(ids) {
     return new Promise((resolve, reject) => {
         if (ids && ids.length && ids.length > 0) {
-            const url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${API_KEY}&steamids=${ids.map(convertTo64BitId).join(",")}`;
+            const url = `${PLAYER_SUMMARIES_API_URL}/?key=${API_KEY}&steamids=${ids.map(convertTo64BitId).join(",")}`;
             return request(url, (error, response, body) => {
                 if (error) {
                     throw new Error(error);
@@ -61,7 +75,7 @@ function isDirectory(path) {
  * @returns
  */
 function getConfigFiles(path) {
-    return fs.readdirSync(path).filter(name => configFiles.indexOf(name) !== -1);
+    return fs.readdirSync(path).filter(name => configFiles.indexOf(name) !== -1).map(value => path_1.join(path, value));
 }
 /**
  * Utility function to get directories of a given path
@@ -79,48 +93,137 @@ function copyFile(source, destination) {
         readStream.pipe(writeStream);
     }
 }
-function main() {
-    const configDirs = getDirectories(userDataPath);
-    const files = {};
-    const playerSummaries = {};
-    getPlayerSummaries(configDirs).then(response => {
-        response.forEach(summary => {
-            playerSummaries[summary.steamid] = summary;
-        });
-        const choices = [];
-        configDirs.map(value => {
-            files[value] = getConfigFiles(path_1.join(userDataPath, value, cfgRelativePath)).map(name => path_1.join(userDataPath, value, cfgRelativePath));
-            if (playerSummaries[convertTo64BitId(value)]) {
-                choices.push(`${value} [${playerSummaries[convertTo64BitId(value)].personaname}]`);
+function importConfig(web = false) {
+    inquirer.prompt([{
+            type: "input",
+            name: "url",
+            message: "Please enter the url of your config"
+        }]).then((answers) => {
+        request(answers.url, (error, response, body) => {
+            if (error) {
+                throw new Error(error);
             }
-            else {
-                choices.push(value);
+            console.log(body);
+        });
+    });
+}
+/**
+ * Export all configs to single json string
+ *
+ */
+function exportConfig() {
+    const exportObject = {};
+    selectConfigDir("Which config would you like to export?").then(result => {
+        const configPath = path_1.join(userDataPath, result, cfgRelativePath);
+        const configFiles = getConfigFiles(configPath);
+        configFiles.forEach(file => {
+            const filename = path_1.basename(file);
+            exportObject[filename] = fs.readFileSync(file, "utf8");
+        });
+        // Write serialized config file out
+        fs.writeFileSync("out.json", JSON.stringify(exportObject));
+    });
+}
+/**
+ * Sync all configs from selected config
+ *
+ */
+function sync() {
+    selectConfigDir("Which config would you like to sync from?").then(result => {
+        confirm("Are you sure you want to sync?").then(answer => {
+            const configPath = path_1.join(userDataPath, result, cfgRelativePath);
+            const configFiles = getConfigFiles(configPath);
+            const files = {};
+            configFiles.forEach(file => {
+                const filename = path_1.basename(file);
+                files[filename] = fs.readFileSync(file, "utf8");
+            });
+            if (answer) {
+                let configDirs = getDirectories(userDataPath);
+                console.log(configDirs);
             }
         });
-        inquirer.prompt([{
-                type: "list",
-                name: "source",
-                message: "Which config would you like to copy",
-                choices
-            }]).then(answers => {
-            const id = answers.source.split(" ")[0];
-            configDirs.filter(name => name !== id).forEach(config => {
-                console.log(files[config]);
+    });
+}
+function confirm(message) {
+    return new Promise((resolve, reject) => {
+        inquirer.prompt({
+            type: "confirm",
+            name: "confirm",
+            message
+        }).then(answer => {
+            resolve(answer.confirm);
+        });
+    });
+}
+function selectConfigDir(message) {
+    return new Promise((resolve, reject) => {
+        let configDirs = getDirectories(userDataPath);
+        const files = {};
+        const playerSummaries = {};
+        getPlayerSummaries(configDirs).then(response => {
+            response.forEach(summary => {
+                playerSummaries[summary.steamid] = summary;
+            });
+            const choices = [];
+            configDirs = configDirs.map(value => {
+                files[value] = getConfigFiles(path_1.join(userDataPath, value, cfgRelativePath)).map(name => path_1.join(userDataPath, value, cfgRelativePath));
+                if (playerSummaries[convertTo64BitId(value)]) {
+                    choices.push(`${value} [${playerSummaries[convertTo64BitId(value)].personaname}]`);
+                }
+                else {
+                    choices.push(value);
+                }
+                return path_1.join(userDataPath, value, cfgRelativePath);
+            });
+            inquirer.prompt([{
+                    type: "list",
+                    name: "source",
+                    message,
+                    choices
+                }]).then(answers => {
+                const id = answers.source.split(" ")[0];
+                resolve(id);
             });
         });
     });
 }
+function main(option) {
+    switch (option) {
+        case applicationOptions.EXPORT:
+            exportConfig();
+            break;
+        case applicationOptions.IMPORT:
+            importConfig();
+            break;
+        case applicationOptions.IMPORTWEB:
+            importConfig(true);
+            break;
+        case applicationOptions.SYNC:
+            sync();
+            break;
+    }
+}
+const choices = [
+    {
+        name: "sync",
+        value: applicationOptions.SYNC
+    },
+    {
+        name: "import",
+        value: applicationOptions.IMPORT
+    },
+    {
+        name: "export",
+        value: applicationOptions.EXPORT
+    },
+];
 inquirer.prompt([{
         type: "list",
-        name: "source",
-        message: "Where are the source configs?",
-        choices: [
-            "copy",
-            "gist",
-            "file"
-        ]
-    }]).then(answers => {
-    console.log(answers);
-    main();
+        name: "option",
+        message: "Please select one",
+        choices: choices
+    }]).then((answers) => {
+    main(answers.option);
 });
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,31 @@
-
 import * as request from "request";
 import * as fs from "fs";
 
 import * as inquirer from "inquirer";
 
-import { join } from "path";
+import { join, basename } from "path";
 
 const APP_ID = 730;
 const API_KEY = "1578879989BD74A6D189050250810E86";
 const BIT_SHIFT: number = 61197960265728;
+const PLAYER_SUMMARIES_API_URL = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002";
+
+const resources = {
+    import: "import",
+    export: "export",
+    sync: "sync"
+}
+
+interface IConfigObject {
+    [key: string]: string;
+}
+
+enum applicationOptions {
+    IMPORT = "IMPORT",
+    IMPORTWEB = "IMPORTWEB",
+    EXPORT = "EXPORT",
+    SYNC = "SYNC"
+}
 
 /**
  * Utility function to convert 32bit steamids to 64bit
@@ -20,6 +37,7 @@ function convertTo64BitId(id: string): string {
     return `765${Number(id) + BIT_SHIFT}`;
 }
 
+// TODO: Add more search paths
 const userDataPath = "C:/Program Files (x86)/Steam/userdata";
 const cfgRelativePath = `${APP_ID}/local/cfg`;
 const configFiles = [
@@ -37,7 +55,7 @@ const configFiles = [
 function getPlayerSummaries(ids: string[]): Promise<any[]> {
     return new Promise((resolve, reject) => {
         if (ids && ids.length && ids.length > 0) {
-            const url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${API_KEY}&steamids=${ids.map(convertTo64BitId).join(",")}`;
+            const url = `${PLAYER_SUMMARIES_API_URL}/?key=${API_KEY}&steamids=${ids.map(convertTo64BitId).join(",")}`;
             return request(url, (error, response, body) => {
                 if (error) {
                     throw new Error(error);
@@ -67,7 +85,7 @@ function isDirectory(path: string): boolean {
  * @returns 
  */
 function getConfigFiles(path: string): string[] {
-    return fs.readdirSync(path).filter(name => configFiles.indexOf(name) !== -1);
+    return fs.readdirSync(path).filter(name => configFiles.indexOf(name) !== -1).map(value => join(path, value));
 }
 
 /**
@@ -80,73 +98,162 @@ function getDirectories(path: string): string[] {
     return fs.readdirSync(path).filter(name => isDirectory(join(userDataPath, name)));
 }
 
-function copyFile(source: string, destination: string): void {
-    const readStream = fs.createReadStream(source);
-    const writeStream = fs.createWriteStream(destination);
-    if (readStream && writeStream) {
-        readStream.pipe(writeStream);
-    }
-}
-
-function main() {
-    let configDirs = getDirectories(userDataPath);
-
-    const files: any = {};
-    const playerSummaries: any = {};
-
-    getPlayerSummaries(configDirs).then(response => {
-        response.forEach(summary => {
-            playerSummaries[summary.steamid] = summary;
-        });
-
-        const choices: string[] = [];
-        configDirs = configDirs.map(value => {
-            files[value] = getConfigFiles(join(userDataPath, value, cfgRelativePath)).map(name => join(userDataPath, value, cfgRelativePath));
-            if (playerSummaries[convertTo64BitId(value)]) {
-                choices.push(`${value} [${playerSummaries[convertTo64BitId(value)].personaname}]`);
+function importConfig(web: boolean = false): void {
+    inquirer.prompt([{
+        type: "input",
+        name: "url",
+        message: "Please enter the url of your config"
+    }]).then((answers) => {
+        request(answers.url as string, (error, response, body) => {
+            if (error) {
+                throw new Error(error);
             }
-            else {
-                choices.push(value);
-            }
-            return join(userDataPath, value, cfgRelativePath);
-        });
-        inquirer.prompt([{
-            type: "list",
-            name: "source",
-            message: "Which config would you like to copy",
-            choices
-        }]).then(answers => {
-
-            const id = answers.source.split(" ")[0];
-
-            configDirs.filter(name => name !== id).forEach(config => {
-                console.log(files[config]);
-            })
+            console.log(body);
         });
     });
 }
 
+/**
+ * Export all configs to single json string
+ * 
+ */
+function exportConfig(): void {
+
+    const exportObject: IConfigObject = {};
+
+    selectConfigDir("Which config would you like to export?").then(result => {
+        const configPath = join(userDataPath, result, cfgRelativePath);
+        const configFiles = getConfigFiles(configPath);
+
+        configFiles.forEach(file => {
+            const filename = basename(file);
+            exportObject[filename] = fs.readFileSync(file, "utf8");
+        });
+
+        // Write serialized config file out
+        fs.writeFileSync("out.json", JSON.stringify(exportObject));
+    });
+}
+
+/**
+ * Sync all configs from selected config
+ * 
+ */
+function sync(): void {
+    selectConfigDir("Which config would you like to sync from?").then(result => {
+        confirm("Are you sure you want to sync?").then(confirmation => {
+            const configPath = join(userDataPath, result, cfgRelativePath);
+            const configFiles = getConfigFiles(configPath);
+
+            const files: IConfigObject = {};
+
+            // Grab all the config files from the source directory
+            configFiles.forEach(file => {
+                const filename = basename(file);
+                files[filename] = fs.readFileSync(file, "utf8");
+            });
+
+            // Write this file to all other config directories
+            if (confirmation) {
+                getDirectories(userDataPath).forEach(value => {
+                    const tempPath = join(userDataPath, value, cfgRelativePath);
+                    for (const file in files) {
+                        fs.writeFileSync(file, files[file]);
+                    }
+                });
+            }
+        })
+    });
+}
+
+function confirm(message: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        inquirer.prompt({
+            type: "confirm",
+            name: "confirm",
+            message
+        }).then(answer => {
+            resolve(answer.confirm);
+        });
+    });
+}
+
+function selectConfigDir(message: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        let configDirs = getDirectories(userDataPath);
+
+        const files: any = {};
+        const playerSummaries: any = {};
+
+        getPlayerSummaries(configDirs).then(response => {
+            response.forEach(summary => {
+                playerSummaries[summary.steamid] = summary;
+            });
+
+            const choices: string[] = [];
+            configDirs = configDirs.map(value => {
+                files[value] = getConfigFiles(join(userDataPath, value, cfgRelativePath)).map(name => join(userDataPath, value, cfgRelativePath));
+                if (playerSummaries[convertTo64BitId(value)]) {
+                    choices.push(`${value} [${playerSummaries[convertTo64BitId(value)].personaname}]`);
+                }
+                else {
+                    choices.push(value);
+                }
+                return join(userDataPath, value, cfgRelativePath);
+            });
+
+            inquirer.prompt([{
+                type: "list",
+                name: "source",
+                message,
+                choices
+            }]).then(answers => {
+                const id = answers.source.split(" ")[0];
+                resolve(id);
+            });
+        });
+    });
+}
+
+function main(option: applicationOptions) {
+    switch (option) {
+        case applicationOptions.EXPORT:
+            exportConfig();
+            break;
+        case applicationOptions.IMPORT:
+            importConfig();
+            break;
+        case applicationOptions.IMPORTWEB:
+            importConfig(true);
+            break;
+        case applicationOptions.SYNC:
+            sync();
+            break;
+    }
+}
+
 const choices: inquirer.ChoiceType[] = [
     {
-        name: "copy",
-        value: "copy"
+        name: "sync",
+        value: applicationOptions.SYNC
     },
     {
-        name: "copy",
-        value: "copy"
+        name: "import",
+        value: applicationOptions.IMPORT
     },
     {
-        name: "copy",
-        value: "copy"
+        name: "export",
+        value: applicationOptions.EXPORT
     },
-]
+];
+
 
 inquirer.prompt([{
     type: "list",
-    name: "source",
-    message: "Where are the source configs?",
+    name: "option",
+    message: "Please select one",
     choices: choices
 }]).then((answers) => {
-    main();
+    main(answers.option);
 });
 
